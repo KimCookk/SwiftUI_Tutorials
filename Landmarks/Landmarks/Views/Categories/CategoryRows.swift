18fae27500095a147f4989510db8093a8ad714ac
Kim TaeSung <fopc4131@gmail.com>
2023-10-19 22:29:02 +0900

    Landmark App update 'Composing complex interfaces' section

---
+++ Landmarks/Landmarks/Views/Categories/CategoryRows.swift
@@ -0,0 +1,43 @@
+//
+//  CategoryRows.swift
+//  Landmarks
+//
+//  Created by 김태성 on 10/19/23.
+//
+
+import SwiftUI
+
+struct CategoryRows: View {
+    var categoryName: String
+    var items: [Landmark]
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text(categoryName)
+                .font(.headline)
+                .padding(.leading, 15)
+                .padding(.top, 5)
+            
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack(alignment: .top, spacing: 0) {
+                    ForEach(items) { landmark in
+                        NavigationLink {
+                            LandmarkDetail(landmark: landmark)
+                        } label: {
+                            CategoryItem(landmark: landmark)
+                        }
+                    }
+                }
+            }
+            .frame(height: 185)
+        }
+    }
+}
+
+#Preview {
+    let landmarks = ModelData().landmarks
+    return CategoryRows(
+        categoryName: landmarks[0].category.rawValue,
+        items: Array(landmarks.prefix(4))
+    )
+}
