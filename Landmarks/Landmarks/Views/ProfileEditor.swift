e81401ce6cd890c6923dd1227a5f9ed086e16cb4
Kim TaeSung <fopc4131@gmail.com>
2023-10-26 23:38:46 +0900

    Landmark App update 'Working with UI controls' section

---
+++ Landmarks/Landmarks/Views/ProfileEditor.swift
@@ -0,0 +1,50 @@
+//
+//  ProfileEditor.swift
+//  Landmarks
+//
+//  Created by 김태성 on 10/26/23.
+//
+
+import SwiftUI
+
+struct ProfileEditor: View {
+    @Binding var profile: Profile
+    var dateRange: ClosedRange<Date> {
+        let min = Calendar.current.date(byAdding: .year, value: -1, to: profile.goalDate)!
+        let max = Calendar.current.date(byAdding: .year, value: 1, to: profile.goalDate)!
+        return min...max
+    }
+    
+    var body: some View {
+        List {
+            HStack {
+                Text("Username").bold()
+                Divider()
+                TextField("Username", text: $profile.username)
+            }
+            
+            Toggle(isOn: $profile.profersNotifications) {
+                Text("Enable Notifications").bold()
+            }
+            
+            VStack(alignment: .leading, spacing: 20) {
+                Text("Seasonal Photo").bold()
+                
+                Picker("Seasonal Photo", selection: $profile.sesonalPhoto) {
+                    ForEach(Profile.Season.allCases) { season in
+                        Text(season.rawValue).tag(season)
+                    }
+                }
+                .pickerStyle(.segmented)
+            }
+            
+            DatePicker(selection: $profile.goalDate, in: dateRange, displayedComponents: .date) {
+                Text("Goal Date").bold()
+            }
+        }
+    }
+}
+
+#Preview {
+    ProfileEditor(profile: .constant(.default))
+}
---
+++ Landmarks/Landmarks/Views/ProfilesSummary.swift
@@ -0,0 +1,62 @@
+//
+//  ProfilesSummary.swift
+//  Landmarks
+//
+//  Created by 김태성 on 10/26/23.
+//
+
+import SwiftUI
+
+struct ProfilesSummary: View {
+    @Environment(ModelData.self) var modelData
+    var profile: Profile
+    
+    var body: some View {
+        ScrollView {
+            VStack(alignment: .leading, spacing: 10) {
+                Text(profile.username)
+                    .bold()
+                    .font(.title)
+                
+                Text("Notifications: \(profile.profersNotifications ? "On" : "Off")")
+                Text("Seasonal Photos: \(profile.sesonalPhoto.rawValue)")
+                Text("Goal Date: ") + Text(profile.goalDate, style: .date)
+                
+                Divider()
+                
+                VStack(alignment: .leading) {
+                    Text("Completed Badges")
+                        .font(.headline)
+                    
+                    ScrollView(.horizontal) {
+                        HStack {
+                            HikeBadge(name: "First Hike")
+                            
+                            HikeBadge(name: "Earth Day")
+                                .hueRotation(Angle(degrees: 90))
+                            
+                            HikeBadge(name: "Tenth Hike")
+                                .grayscale(0.5)
+                                .hueRotation(Angle(degrees: 45))
+                        }
+                        .padding(.bottom)
+                    }
+                }
+                
+                Divider()
+                
+                VStack(alignment: .leading) {
+                    Text("Recent Hikes")
+                        .font(.headline)
+                    
+                    HikeView(hike: modelData.hikes[0])
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    ProfilesSummary(profile: Profile.default)
+        .environment(ModelData())
+}
